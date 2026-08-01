5524557064c38887ad5f0c622b2bb5d0155877c5
qamotech <qamotech@gmail.com>
2026-07-31 20:25:15 -0400

    Deploy 2040 Arcade Suite with full WASM core

--- Ev2040.service.worker.js
+++ Ev2040.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785479344|367388887';
+const CACHE_VERSION = '1785537479|5388589593';
 /** @type {string} */
 const CACHE_PREFIX = 'Evony Runner: Ap-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
